28721da05620bb1316fab467f648b04de80e9434
Marta Adarve de Leon <martaadl@kth.se>
2024-12-03 11:58:03 +0100

    Define model

--- src/model/TTPModel.js
+++ src/model/TTPModel.js
@@ -1,5 +1,32 @@
+import { getUserHistory,getSummaryDetails} from "../userSource";
+import { resolvePromise } from "../resolvePromise";
 const model = {
-    exampleData: "Hello World"
+    currentUserId: null,
+    searchURLs: [],
+    currentSummaryId: null,
+    summaryPromiseState: {},
+    currentUserPromiseState: {},
+    setCurrentUserId(userId){
+        if (userId && this.currentUserId!==userId){
+            this.currentUserId = userId;
+            resolvePromise(getUserHistory(userId),this.currentUserPromiseState);
+        }
+    },
+    setCurrentSummaryId(summaryId){
+        if (summaryId && this.currentSummaryId!==summaryId){
+            this.currentSummaryId = summaryId;
+            resolvePromise(getSummaryDetails(currentUserId,summaryId),this.summaryPromiseState);
+        }
+    },
+    addURL(URLToAdd){
+        this.searchURLs= [...this.searchURLs,URLToAdd];
+    },
+    removeURL(URLToRemove){
+        function shouldWeKeepURLCB(URL){
+            return URLToRemove !== URL;
+        }
+        this.searchURLs = this.searchURLs.filter(shouldWeKeepURLCB);
+    }
 }
 
 export {model}; // export the model
