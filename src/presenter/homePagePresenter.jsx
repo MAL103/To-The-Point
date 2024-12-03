bebf570ce81d3b493465eb2dc46acb218a4fd008
caitlinym <127906520+caitlinym@users.noreply.github.com>
2024-12-03 16:02:07 +0100

    Initial setup of the homepage

---
+++ src/presenter/homePagePresenter.jsx
@@ -0,0 +1,10 @@
+import {HomePageView} from "../view/homePageView.jsx";
+import {observer} from "mobx-react-lite";
+
+
+
+const HomePagePresenter = observer(function HomePagePresenter(props){
+    return <HomePageView exampleData={props.model.exampleData} />
+})
+
+export {HomePagePresenter};
