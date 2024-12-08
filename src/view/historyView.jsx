01adf66973acabe148216cd347f23ec4dd0cc798
Marta Adarve de Leon <martaadl@kth.se>
2024-12-08 22:49:35 +0100

    Initial setup of the history

---
+++ src/view/historyView.jsx
@@ -0,0 +1,12 @@
+import "../style.css"
+
+export function HistoryView(props) {
+    return (<div class="sidebar">
+        <h2>Article History</h2>
+        <ul>
+            <li>Article 1</li>
+            <li>Article 2</li>
+            <li>Article 3</li>
+        </ul>
+    </div>);
+}
