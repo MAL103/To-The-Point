c402fc28bc277bb0d9909a307a001e396ca9b558
Zvonimir Bednarcik <34624302+Skittish-h@users.noreply.github.com>
2024-12-09 18:20:23 +0100

    Add functional summarizeContent.js (#12)
    Fix env var deployment

--- src/presenter/loginPresenter.jsx
+++ src/presenter/loginPresenter.jsx
@@ -1,8 +1,15 @@
 import {LoginView} from "../view/loginView.jsx";
 import {observer} from "mobx-react-lite";
+import {summarizeContent} from "../util/summarizeContent";
 
 const LoginPresenter = observer(function LoginPresenter(props){
   // TO DO - connect the model and put actual data in here
+   summarizeContent("here is some example content. Syrian Arab Republic, 2019. " +
+       "The Syrian Arab Republic is a country in Western Asia, bordering Lebanon, Turkey, Iraq, Jordan").then((data) => {
+           console.log(data);
+       }
+   );
+
     return <LoginView/>
 })
 
