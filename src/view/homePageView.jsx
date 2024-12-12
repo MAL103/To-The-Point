e79ab8b2352e118f6ff45b96a8e5ffd1d15d58f2
caitlinym <127906520+caitlinym@users.noreply.github.com>
2024-12-12 14:20:56 +0100

    initial attempt at summarise content button

--- src/view/homePageView.jsx
+++ src/view/homePageView.jsx
@@ -1,15 +1,33 @@
 import "../style.css"
+import { fetchUrlContent } from "../util/fetchUrlContent"
+import {useState} from "react"
+import { summarizeContent } from "../util/summarizeContent";
 
 export function HomePageView(props) {
+
+  // is this ok to be doing? should i be using the presenter??
+  const [url, setUrl] = useState("");
+
+  function summariseContentACB() {
+    console.log(url);
+    console.log(fetchUrlContent(url));
+    console.log(summarizeContent(fetchUrlContent(url)));
+  }
+
   return(
     <div>
       <div className="body">
         <h1 className="title">get to <span className="the">the</span> point</h1>
         <h2>Get the key concepts of your news articles</h2>
         <div className="searchContainer">
-          <input className="inputBar" placeholder="Paste a URL here"></input>
-          {/* The on click for this button will call API */}
-          <button className="search-button">
+          <input
+            className="inputBar"
+            placeholder="Paste a URL here"
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+          >
+          </input>
+          <button className="search-button" onClick={summariseContentACB}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" width="20" height="20">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 15l7-7 7 7" />
             </svg>
