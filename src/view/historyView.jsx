a6949f742d275aadff26e4328694f365186b5691
Marta Adarve de Leon <martaadl@kth.se>
2024-12-16 12:03:15 +0100

    History view simplified

--- src/view/historyView.jsx
+++ src/view/historyView.jsx
@@ -9,15 +9,9 @@ export function HistoryView(props) {
             </tr>
         );
     }
-    function historyRowCB(row){
-        return <li>Key Concept</li>
-    }
+
     return (<div className="sidebar">
         <h2>Article History</h2>
-        <ul>
-            {props.exampleData.map(historyRowCB)}
-        </ul>
-        <h3>Dummy data persisted from firebase</h3>
         <table>
           <tbody>
             {(props.summaries).map(summariesRenderingACB)}
