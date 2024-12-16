41c4ad2b2f47facd5d386dc4d08d65717a12d876
caitlinym <127906520+caitlinym@users.noreply.github.com>
2024-12-16 16:31:11 +0100

    merge conflicts

--- src/presenter/homePagePresenter.jsx
+++ src/presenter/homePagePresenter.jsx
@@ -1,11 +1,11 @@
-import { summarizeContent } from "../util/summarizeContent.js";
+// import { summarizeContent } from "../util/summarizeContent.js";
 import {useState} from "react"
-import { fetchUrlContent } from "../util/fetchUrlContent"
+// import { fetchUrlContent } from "../util/fetchUrlContent"
 import {HomePageView} from "../view/homePageView.jsx";
 import {observer} from "mobx-react-lite";
 
 const HomePagePresenter = observer(function HomePagePresenter(props){
-    const [url, setUrl] = useState("");
+    // const [url, setUrl] = useState("");
 
     function setModelUrl(url) {
         props.model.url=url;
