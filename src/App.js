5a0044f3c1372dec7cb3651111a4ff176cc7368c
Marta Adarve de Leon <martaadl@kth.se>
2024-12-17 10:23:39 +0100

    Router modified

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import {createHashRouter, RouterProvider} from "react-router-dom";
 import { HomePagePresenter } from './presenter/homePagePresenter';
 import { LoginPresenter } from './presenter/loginPresenter';
-import { HistoryPresenter } from './presenter/historyPresenter';
+import { HistorySummaryPresenter } from './presenter/historySummaryPresenter';
 import { ProfileLogOutPresenter } from './presenter/profileLogOutPresenter';
 import { observer } from "mobx-react-lite";
 import { NavigationBarPresenter } from './presenter/navigationBarPresenter';
