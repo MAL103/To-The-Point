6a51a685b9902cced2030fa5dbd4f08d1404b64f
Skittish-h <zvonko2779@gmail.com>
2024-12-17 12:04:26 +0100

    Massive humongous change to make markdown work

--- src/view/summaryView.jsx
+++ src/view/summaryView.jsx
@@ -1,3 +1,5 @@
+import Markdown from "react-markdown";
+
 export function SummaryView(props){
     return (
         <div className="summary">
@@ -7,7 +9,7 @@ export function SummaryView(props){
             <p></p>
             {props.summaryId !==null && props.summaries && props.summaries[props.summaryId] ? (
             <div>
-                <p>{props.summaries[props.summaryId].summary}</p>
+                <Markdown>{props.summaries[props.summaryId].summary}</Markdown>
             </div>
             ) : (
             <p>Select a summary</p>
