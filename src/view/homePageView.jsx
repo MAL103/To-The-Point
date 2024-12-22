37fa7b37e5a7a486b882f26edbab9999f31f50f5
Zvonimir Bednarcik <34624302+Skittish-h@users.noreply.github.com>
2024-12-22 22:58:56 +0100

    Extract titles (#58)
    * extract title from context
    
    * Add enw summary button
    
    * add error handling
    
    * remove fluff

--- src/view/homePageView.jsx
+++ src/view/homePageView.jsx
@@ -4,27 +4,62 @@ export function HomePageView(props) {
 
   function onSummarizeClickACB() {
     props.onSummarizeClick();
-  };
+  }
 
-  return(
-    <div>
-      <div className="body">
-        <h1 className="title">get to <span className="the">the</span> point</h1>
-        <h2>Get the key concepts of your news articles</h2>
-        <div className="searchContainer">
-          <input
-            className="inputBar"
-            placeholder="Paste a URL here"
-            onChange={(e) => props.setUrl(e.target.value)}
-          >
-          </input>
-          <button className="search-button" onClick={onSummarizeClickACB}>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" width="20" height="20">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7" />
-            </svg>
-          </button>
+  return (
+      <div>
+        <div className="body">
+          <h1 className="title">
+            get to <span className="the">the</span> point
+          </h1>
+          <h2>Get the key concepts of your news articles</h2>
+          {props.loading ? (
+              <img
+                  src="https://brfenergi.se/iprog/loading.gif"
+                  alt="loading img"
+                  className="loading-image"
+              />
+          ) : (
+              <div
+                  className={
+                    props.error
+                        ? "searchContainer searchContainer-error"
+                        : "searchContainer"
+                  }
+              >
+                <input
+                    className={
+                      props.error ? "inputBar inputBar-error" : "inputBar"
+                    }
+                    placeholder="Paste a URL here"
+                    onChange={(e) => props.setUrl(e.target.value)}
+                />
+                <button className="search-button" onClick={onSummarizeClickACB}>
+                  <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      width="20"
+                      height="20"
+                  >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M9 5l7 7-7 7"
+                    />
+                  </svg>
+                </button>
+              </div>
+          )}
+          {props.error ? (
+              <p className="inputBar-error-text">
+                Some websites block access to websites like To The Point, check if
+                your URL is valid or try another one.
+              </p>
+          ) : null}
         </div>
       </div>
-    </div>
-  )
+  );
 }
